c9b55829ee794454b7e8f6ed21de0da527019282
Exequiel Arroyo <zekielarroyo@gmail.com>
2023-03-26 15:03:07 +0800

    Added tree keybind

--- .config/nvim/lua/exequielarroyo/init.lua
+++ .config/nvim/lua/exequielarroyo/init.lua
@@ -3,8 +3,11 @@ print('Exequiel Arroyo')
 
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>n', ":BufferLineCycleNext<CR>")
+--vim.keymap.set('n', '<C-l>', ":BufferLineCycleNext<CR>")
+--vim.keymap.set('n', '<C-h>', ":BufferLineCyclePrev<CR>")
 vim.keymap.set('n', '<leader>d', ":BufferLineCloseRight<CR>")
+vim.keymap.set('n', '<leader>b', ":BufferLinePick<CR>")
+vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>")
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
