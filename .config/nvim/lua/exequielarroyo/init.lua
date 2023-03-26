b9866f508ebb42dfe56890e6a42c22e5032ccec8
Exequiel Arroyo <zekielarroyo@gmail.com>
2023-03-26 15:19:53 +0800

    Added gitsigns keybindings

--- .config/nvim/lua/exequielarroyo/init.lua
+++ .config/nvim/lua/exequielarroyo/init.lua
@@ -8,6 +8,8 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>d', ":BufferLineCloseRight<CR>")
 vim.keymap.set('n', '<leader>b', ":BufferLinePick<CR>")
 vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>")
+vim.keymap.set('n', '<C-h>', ":Gitsigns preview_hunk<CR>")
+vim.keymap.set('n', '<C-s>', ":Gitsigns stage_hunk<CR>")
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
