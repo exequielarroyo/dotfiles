d832942b01cfcfb8a8161eb346e9db7f4323be5e
CORP\exequiel.arroyo <zekielarroyo@gmail.com>
2024-11-04 17:06:33 +0800

    Added neovim config

--- .config/no-plugin/lua/settings.lua
+++ .config/no-plugin/lua/settings.lua
@@ -6,10 +6,26 @@ vim.opt.cinoptions = 'eN' -- not sure how it works
 vim.opt.cursorcolumn = false
 vim.opt.cursorline = false
 vim.opt.errorbells = true
-vim.opt.expandtab = true                 -- use 'CTRL-V<Tab>' to insert real tab when true
+vim.opt.expandtab = true                -- use 'CTRL-V<Tab>' to insert real tab when true
 -- vim.opt.foldcolumn = 'auto'
 vim.opt.foldenable = true
 vim.opt.hlsearch = true
+vim.opt.icon = true
+vim.opt.iconstring = '😀'
+vim.opt.keywordprg = ':Man'
+-- vim.opt.loadplugins = false          -- this disables netrw
+vim.opt.mouse = 'n'
+vim.opt.number = true
+vim.opt.numberwidth = 1
+vim.opt.pumblend = 0                    -- pupup-menu transparency
+vim.opt.pumwidth = 15
+vim.opt.relativenumber = true
+vim.opt.ruler = false
+vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 4                  -- number of spaces used in <Tab>
+vim.opt.showtabline = 2                 -- display tab
+vim.opt.sidescrolloff = 8
+vim.opt.signcolumn = 'auto'             -- a way to resize lsp sign column "auto:[1-2]"
 
 vim.opt.visualbell = true
 
